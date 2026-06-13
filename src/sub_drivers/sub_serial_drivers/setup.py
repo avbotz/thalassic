44142af43444eefc44421eda742cf296c9590e50
Kethan Vegunta <kethan@vegunta.com>
2026-06-13 15:55:05 -0700

    fix(sub_low): transition to python and pyserial

---
+++ src/sub_drivers/sub_serial_drivers/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = "sub_serial_drivers"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools", "pyserial"],
+    zip_safe=True,
+    maintainer="AVBotz",
+    maintainer_email="avbotzco@gmail.com",
+    description="Serial bridge drivers (sub_low board, NaviGuider IMU) for the Marlin V2 AUV.",
+    license="Proprietary",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "sub_low = sub_serial_drivers.sub_low:main",
+            "naviguider_imu_driver = sub_serial_drivers.naviguider_imu_driver:main",
+        ],
+    },
+)
