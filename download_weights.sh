2a4f5dd37882397723908cfa62ee275786bd350e
Kethan Vegunta <kethan@vegunta.com>
2026-07-13 09:45:54 -0700

    fix: model loading path

--- download_weights.sh
+++ download_weights.sh
@@ -3,15 +3,25 @@
 mkdir -p weights
 
 declare -A sim_models=(
-    ["gate"]="https://drive.google.com/file/d/1811hLDDxDxHMyztwxl88FgQLGjd98Rpu/view?usp=sharing"
-    ["slalom"]="https://drive.google.com/file/d/1fpKPookhjDVDDNKeEgshl0-yfyH-V0jp/view?usp=sharing"
-    ["torp"]="https://drive.google.com/file/d/10ptY-xR_U4myQR5u6esSzd5Hgt2FNk6H/view?usp=sharing"
+    ["gate"]="https://drive.google.com/file/d/1811hLDDxDxHMyztwxl88FgQLGjd98Rpu/view"
+    ["slalom"]="https://drive.google.com/file/d/1fpKPookhjDVDDNKeEgshl0-yfyH-V0jp/view"
+    ["torp"]="https://drive.google.com/file/d/10ptY-xR_U4myQR5u6esSzd5Hgt2FNk6H/view"
+    ["bins_blood"]="https://drive.google.com/file/d/1Ddo-M3zvYALfrasv8MyDgGH87v2yXbon/view"
+    ["bins_fire"]="https://drive.google.com/file/d/1Ls-4B011pH3gqthPT8Ppgnmjf74c1qOn/view"
+    ["path_marker"]="https://drive.google.com/file/d/1Z48v2QMXTAKXQeYasLrr7ppSr43AZk7q/view"
+    ["slalom"]="https://drive.google.com/file/d/1fpKPookhjDVDDNKeEgshl0-yfyH-V0jp/view"
+    ["octagon_images"]="https://drive.google.com/file/d/1syv_VMwoPr9ugpACBvf9PaHqhQtNTHWb/view"
 )
 
 declare -A models=(
     ["gate"]=""
     ["slalom"]=""
     ["torp"]=""
+    ["bins_blood"]=""
+    ["bins_fire"]=""
+    ["path_marker"]=""
+    ["slalom"]=""
+    ["octagon_images"]=""
 )
 
 if [[ " $* " =~ " --sim " ]]; then
--- src/sub_pid_tuner/web/app.js
+++ src/sub_pid_tuner/web/app.js
@@ -41,6 +41,14 @@ function setPill(id, text, kind) {
   el.className = "pill " + (kind || "");
 }
 
+if (typeof window !== 'undefined' && window.crypto && !window.crypto.randomUUID) {
+  window.crypto.randomUUID = function () {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  };
+}
+
 function request(type, payload) {
   if (!state.socket || state.socket.readyState !== WebSocket.OPEN) {
     return Promise.reject(new Error("Dashboard is disconnected"));
